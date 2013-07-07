b3562684733d4495eaab83cc6b31055435c552c1
INABA Hitoshi <ina@cpan.org>
2013-07-07 14:33:07 +0000

    Import of INA/Char-KPS9566-0.91 from CPAN.
    gitpan-cpan-distribution: Char-KPS9566
    gitpan-cpan-version:      0.91
    gitpan-cpan-path:         INA/Char/KPS9566/Char-KPS9566-0.91.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Char/KPS9566.pm
+++ Char/KPS9566.pm
@@ -29,7 +29,7 @@ BEGIN {
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.90 $ =~ /(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.91 $ =~ /(\d+)/oxmsg;
 
 BEGIN { CORE::require Char::Ekps9566; }
 
@@ -500,7 +500,7 @@ sub Char::KPS9566::escape_script {
         # in Chapter 5: Pattern Matching
         # of ISBN 978-0-596-00492-7 Programming Perl 4th Edition.
 
-        $e_script .= sprintf("use Char::Ekps9566 %s;\n", $Char::KPS9566::VERSION); # require run-time routines version
+        $e_script .= sprintf("use Char::Ekps9566 %s.0;\n", $Char::KPS9566::VERSION); # require run-time routines version
 
         # use Char::KPS9566 version qw(ord reverse getc);
         $function_ord     = 'ord';
@@ -6819,12 +6819,6 @@ Char/KPS9566.pm removes 'bytes::' at head of function name.
   bytes::substr    substr    Byte
   ---------------------------------------
 
-=head1 Escaping Built-in Standard Module (Char/Ekps9566.pm provides)
-
-Char/Ekps9566.pm does "BEGIN { unshift @INC, '/Perl/site/lib/Char::KPS9566' }" at head.
-Store the standard module modified for Char::KPS9566 software in this directory to
-override built-in standard modules.
-
 =head1 Escaping Standard Module Content (You do)
 
 You need copy built-in standard module to /Perl/site/lib/Char::KPS9566 and change
@@ -7562,3 +7556,4 @@ I am thankful to all persons.
 
 =cut
 
+# Did you fun enough? ^o^)
